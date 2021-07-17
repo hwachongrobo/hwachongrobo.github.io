f1719f9fe89a62ddb388173ddcec34980c6ba7b5
Ivan Tung <ivantung@me.com>
2021-07-17 22:25:46 +0800

    Set up
    Signed-off-by: Ivan Tung <ivantung@me.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,110 +1,108 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Hwa Chong Robotics',
-  tagline: 'Robots are cool',
-  url: 'https://hwachongrobotics.github.io',
-  baseUrl: '/hwachongrobotics.github.io/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  trailingSlash:true,
-  organizationName: 'hwachongroboticss', // Usually your GitHub org/user name.
-  projectName: 'hwachongrobotics.github.io', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
+  title: "Hwa Chong Robotics",
+  tagline: "Robots are cool",
+  organizationName: "hwachongrobo",
+  projectName: "hwachongrobo.github.io",
+  url: "https://hwachongrobo.github.io",
+  baseUrl: "/",
+  trailingSlash: false,
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          path: "docs",
+          routeBasePath: "/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
+  themeConfig: {
+    prism: {
+      theme: require("prism-react-renderer/themes/vsLight"),
+      darkTheme: require("prism-react-renderer/themes/vsDark"),
+    },
+    navbar: {
+      hideOnScroll: true,
+      logo: {
+        alt: "Hwa Chong Robotics Logo",
+        src: "img/logo.png",
+      },
+      title: "Hwa Chong Robotics",
+      items: [
+        {
+          type: "doc",
+          docId: "content",
+          position: "left",
+          label: "Docs",
+        },
+      ],
+    },
+    sidebarCollapsible: true,
+    hideableSidebar: true,
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Lego",
+              to: "/lego/introduction",
+            },
+            {
+              label: "RCJ Soccer Lightweight",
+              to: "/soccer-lightweight/introduction",
+            },
+            {
+              label: "RCJ Soccer Open",
+              to: "/soccer-open/introduction",
+            },
+            {
+              label: "RCJ Rescue Line",
+              to: "/rescue-line/introduction",
+            },
+          ],
+        },
+        {
+          title: "Hwa Chong Robotics",
+          items: [
+            {
+              label: "Website",
+              href: "https://robo.infocommsociety.com",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/hwachongroboticss",
+            },
+          ],
+        },
+        {
+          title: "Transcendence (one of our teams)",
+          items: [
+            {
+              label: "Website",
+              href: "https://bozo.infocommsociety.com/",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/bozotics",
+            },
+            {
+              label: "Instagram",
+              href: "https://instagram.com/bozotics",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Hwa Chong Robotics`,
+    },
+  },
 };
